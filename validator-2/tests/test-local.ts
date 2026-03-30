fdb258e27f5d4be567c3229af1fb6d104eff9564
romelt777 <romel.tan777@gmail.com>
2026-03-30 14:03:52 -0300

    tests passed locally

--- validator-2/tests/test-local.ts
+++ validator-2/tests/test-local.ts
@@ -1,5 +1,9 @@
 import "dotenv/config";
+import AWSXRay from "aws-xray-sdk";
 //environment variables are not auto set
+
+//nned to ignore xray, because test is local only, not running from aws lambda
+AWSXRay.setContextMissingStrategy("IGNORE_ERROR");
 
 import { handler } from "../src/index"
 
@@ -7,17 +11,17 @@ const testEvent = {
     body: JSON.stringify({
         id: 222,
         email: "user@example.com",
-        name: "!",
+        name: "BOB",
         phone: "5091118186",
         quantity: 333,
         creditCard: "4242424242424242",
         expirationDate: "09/67",
         securityCode: "511",
-        address: "str",
-        city: "s",
+        address: "strING",
+        city: "sOUTH CITY",
         province: "string",
         postalCode: "90210",
-        country: "s"
+        country: "SANADA"
     })
 };
 
